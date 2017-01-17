a46bfba5a099f48b1cafbade4e26a841c3cccc0e
Ben Long <14benj@gmail.com>
2017-01-16 23:35:38 -0500

    removing player component. no longer needed

--- webpack/components/components.js
+++ webpack/components/components.js
@@ -4,7 +4,6 @@ import angular from 'angular';
 import Card from './card/card';
 import Game from './game/game';
 import Score from './score/score';
-import Player from './player/player';
 
 
-export default angular.module('app.components', [Card.name, Game.name, Score.name, Player.name]);
+export default angular.module('app.components', [Card.name, Game.name, Score.name]);
--- webpack/components/player/player.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import angular from 'angular';
-import PlayerComponent from './player.component';
-import template from './player.component.html';
-
-
-export default angular.module('player', []).component('playerComponent', PlayerComponent);
